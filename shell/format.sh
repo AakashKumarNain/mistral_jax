ca22b03b52907fb444ed4bd56684b779964c4910
AakashKumarNain <aakashnain@outlook.com>
2024-03-06 13:46:48 +0530

    fix formatting

--- shell/format.sh
+++ shell/format.sh
@@ -4,4 +4,4 @@ set -Eeuo pipefail
 base_dir=$(dirname $(dirname $0))
 
 ruff check . --fix --config "${base_dir}/pyproject.toml" 
-ruff format . --fix --config "${base_dir}/pyproject.toml"
+ruff format . --config "${base_dir}/pyproject.toml"
