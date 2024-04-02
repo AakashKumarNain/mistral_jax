bae052d56de52eafd2049798c7875909df65be4a
AakashKumarNain <aakashnain@outlook.com>
2024-04-02 20:23:40 +0530

    add documentation for weights_util file

--- mistral_jax/weights_utils.py
+++ mistral_jax/weights_utils.py
@@ -4,6 +4,24 @@ import jax.numpy as jnp
 
 
 def load_weights(torch_weights, path, leaf):
+    """Recursively sets the weights of an equinox module
+    from torch `state_dict()`.
+
+    Args:
+        torch_weights: State dict containing weights of the torch model
+        path: A Pytree
+        leaf: Equinox module
+
+    Example usage:
+    eqx_model = ...    
+    eqx_model jax.tree_util.tree_map_with_path(
+        partial(torch_state_dict, load_weights), eqx_model
+    )
+
+    Returns:
+        Pytree with the desired values
+    """
+
     path_pieces = []
     for path_elem in path:
         if isinstance(path_elem, jax.tree_util.GetAttrKey):
